73fad434651db493c67eb3cb380cd23753d48439
k3n3r <Baolan.Shi@colorado.edu>
2025-10-29 23:59:50 +0100

    add: allow a logged-in user to add new blogs

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,12 +19,25 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
     
     try {
       const user = await loginService.login({ username, password })
-      noteService.setToken(user.token)
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -29,6 +46,29 @@ const App = () => {
       setTimeout(() => {
         setErrorMessage(null)
       }, 5000)
+    }
+  }
+
+  const handleBlogChange = (event) => {
+    setNewBlog(event.target.value)
+  }
+
+  const addBlog = async event => {
+    event.preventDefault()
+    const blogObject = {
+      title,
+      author,
+      url,
+    }
+
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(returnedBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (error) {
+      console.error('Error creating blog:', error)
     }
   }
 
@@ -56,11 +96,32 @@ const App = () => {
     </form>
   )
 
-  const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input value={newNote} onChange={handleNoteChange} />
-      <button type="submit">save</button>
-    </form>
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+    <h3>create new</h3>
+    <div>
+      title:
+      <input
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+    </div>
+    <div>
+      author:
+      <input
+        value={author}
+        onChange={({ target }) => setAuthor(target.value)}
+      />
+    </div>
+    <div>
+      url:
+      <input
+        value={url}
+        onChange={({ target }) => setUrl(target.value)}
+      />
+    </div>
+    <button type="submit">save</button>
+  </form>
   )
 
   return (
@@ -70,13 +131,14 @@ const App = () => {
       {user && (
       <div>
         <p>{user.name} logged in</p>
-        {noteForm()}
+        {blogForm()}
+
+        
+        {blogs.map(blog => 
+          <Blog key={blog.id} blog={blog} />
+        )}
       </div>
     )}
-
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
     </div>
   )
 }
