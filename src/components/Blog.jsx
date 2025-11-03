4424847a02f7e24ddeadcb9f91a47e5fb4a74985
k3n3r <Baolan.Shi@colorado.edu>
2025-11-03 18:45:13 +0100

    add: like option

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,21 @@
 import Togglable from './Togglable'
+import blogService from '../services/blogs'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Blog = ({ blog }) => {
+  const [currentBlog, setCurrentBlog] = useState(blog)
+
+  const handleLike = async () => {
+    try {
+      const updatedBlog = await blogService.uplike(blog.id)
+      console.log('Updated blog:', updatedBlog)
+      setCurrentBlog(updatedBlog)
+    } catch (error) {
+      console.error('Error liking the blog:', error)
+    }
+  }
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -14,7 +29,7 @@ const Blog = ({ blog }) => {
       <Togglable buttonLabel="view">
         <p>author: {blog.author}</p>
         <p>url: {blog.url} </p>
-        <p>likes: {blog.likes}</p>
+        <p>likes: {blog.likes} <button onClick={handleLike}>like</button></p>
       </Togglable>
     </div>
   </div>
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -26,4 +26,24 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { getAll, create, update, setToken }
+const uplike = async (id) => {
+  const response = await axios.get(`${baseUrl}/${id}`)
+  const blog = response.data
+  try {
+    const config = {
+    headers: { Authorization: token }
+  }
+    const newObject = {
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: (blog.likes || 0) + 1,
+    }
+    const request = await axios.put(`${baseUrl}/${id}`, newObject, config)
+    return request.data
+  } catch (error) {
+    console.error('Error in uplike:', error.response ? error.response.data : error.message)
+    throw error}
+}
+
+export default { getAll, create, update, setToken, uplike}
