3fcd0c07e38cc0c824e3f9cb42166a6a78915177
k3n3r <Baolan.Shi@colorado.edu>
2025-11-03 16:47:11 +0100

    feat:  inline styles

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,14 +1,23 @@
 import Togglable from './Togglable'
 
-const Blog = ({ blog }) => (
-  <div>
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (<div>
+    <div style={blogStyle}>
     {blog.title} 
-    <Togglable buttonLabel="view">
-      <p>author: {blog.author}</p>
-      <p>url: {blog.url} </p>
-      <p>likes: {blog.likes}</p>
-    </Togglable>
-  </div>  
-)
+      <Togglable buttonLabel="view">
+        <p>author: {blog.author}</p>
+        <p>url: {blog.url} </p>
+        <p>likes: {blog.likes}</p>
+      </Togglable>
+    </div>
+  </div>
+)}
 
 export default Blog
